3dd3d5072ee592f216671e9eda74e81fde32fa3f
DrMkelvo <kelvincent001@gmail.com>
2022-03-22 04:19:41 +0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main -header file to check our functions
+ * swap_int(int *a, int *b) -a function to swap
+ * no return 0
+ */
+ void swap_int(int a*,int *b)
+{
+int x = 0;
+c = a*;
+b* = b*;
+b* = c;
+}
