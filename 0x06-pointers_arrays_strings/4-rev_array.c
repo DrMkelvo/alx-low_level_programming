1c90a061f055cd72e9fe95fba4a19629d553a6b5
DrMkelvo <kelvincent001@gmail.com>
2022-03-26 04:23:36 +0800

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,21 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
+ * reverse_array - reverses the content of an array of integers.
+ * @a: an array of integers
+ * @n: the number of elements to swap
  *
- * Return: void
+ * Return: nothing.
  */
 void reverse_array(int *a, int n)
 {
-	int i;
-	int temp;
+	int i, j, t;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
-		temp = a[i];
-		a[i] = a[n];
-		a[n] = temp;
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
 	}
 }
+
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0x06-pointers_arrays_strings
+A README for 0x06-pointers_arrays_strings
