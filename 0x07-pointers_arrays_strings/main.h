f7bdbfe72ed75a0c9f93d48874393f18a6ebddf7
DrMkelvo <kelvincent001@gmail.com>
2022-03-29 09:08:32 +0800

    readme.md && main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+/* this prototype has to be declared in a header file */
+int _putchar(char c);
+/* this prototype has to be declared in a header file */
+char *_memset(char *s, char b, unsigned int n);
+/* this prototype has to be declared in a header file */
+char *_memcpy(char *dest, char *src, unsigned int n);
+/* this prototype has to be declared in a header file */
+unsigned int _strspn(char *s, char *accept);
+/* this prototype has to be declared in a header file */
+unsigned int _strspn(char *s, char *accept);
+/* this prototype has to be declared in a header file */
+char *_strpbrk(char *s, char *accept);
+/* this prototype has to be declared in a header file */
+char *_strstr(char *haystack, char *needle);
+/* this prototype has to be declared in a header file */
+void print_chessboard(char (*a)[8]);
+/* this prototype has to be declared in a header file */
+void print_diagsums(int *a, int size);
+/* this prototype has to be declared in a header file */
+void set_string(char **s, char *to);
